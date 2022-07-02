9d1e19592c94950ad37b934f927451e46690802d
Hunter Herman <hherman@palantir.com>
2022-07-02 16:16:30 -0400

    fix URLs

--- components.go
+++ components.go
@@ -14,7 +14,7 @@ func putForm(existing persist.Catalog) string {
 	existing.Price.Float64 = math.Round(existing.Price.Float64*100) / 100
 	return fmt.Sprintf(`
 <div class="grid place-items-center">
-<form hx-post="/api/put" hx-target="#viewport" class="w-96 grid grid-cols-1 place-content-center">
+<form hx-post="api/put" hx-target="#viewport" class="w-96 grid grid-cols-1 place-content-center">
 	<input type="hidden" name="id" value="%v" />
 	<label for="title"> Title </label> <input type="text" name="title" class="border-2 p-2" value="%v"> </input> <br/>
 	<label for="description"> Description </label> <br /> <textarea name="description" class="border-2 p-2" value="%v">%v</textarea><br/>
@@ -30,7 +30,7 @@ func putForm(existing persist.Catalog) string {
 	<label for="pattern"> Pattern </label> <input type="text" name="pattern" class="border-2 p-2" value="%v"/> <br/>
 	<label for="price"> Price </label> <input type="text" name="price" class="border-2 p-2" value="%v" placeholder="30.99" /> <br/>
 	<input type="submit"  class="border-2 p-2 rounded-full text-blue-100 bg-blue-600 hover:bg-blue-500 mb-4 cursor-pointer"/>
-	<button class="border-2 p-2 rounded-full bg-slate-50 hover:bg-slate-100" hx-get="/component/list" hx-target="#viewport"> Cancel </button>
+	<button class="border-2 p-2 rounded-full bg-slate-50 hover:bg-slate-100" hx-get="component/list" hx-target="#viewport"> Cancel </button>
 </form>
 </div>`,
 		existing.ID,
@@ -81,9 +81,9 @@ const itemTmpl = `
 		<div class="p-1"> {{.Pattern.String}} </div>
 		<div class="p-1 text-green-800"> ${{printf "%.2f" .Price.Float64}} </div>
 
-		<button hx-target="#viewport" hx-get="/component/putCatalog?id={{.ID}}" class="p-2 text-slate-500 rounded-lg bg-slate-50 hover:bg-slate-100"> Edit </button>
+		<button hx-target="#viewport" hx-get="component/putCatalog?id={{.ID}}" class="p-2 text-slate-500 rounded-lg bg-slate-50 hover:bg-slate-100"> Edit </button>
 		{{- if eq (used .LastActivity.Time) false }}
-		<button hx-target="#list-{{.ID}}" hx-get="/api/use?id={{.ID}}" class="p-2 rounded-lg text-green-600 bg-green-100 hover:bg-green-200"> Use </button>
+		<button hx-target="#list-{{.ID}}" hx-get="api/use?id={{.ID}}" class="p-2 rounded-lg text-green-600 bg-green-100 hover:bg-green-200"> Use </button>
 		{{- end}}
 	</div>`
 
